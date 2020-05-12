c5ce25c9446dbe3979e9c76c3acf06a3618cd546
Jose Sustaita <josesustaita70@gmail.com>
2020-05-12 17:28:57 -0400

    MVP

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import AddFriend from "./AddFriend";
+
+function FriendsList() {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/friends")
+      .then((res) => {
+        console.log(res);
+        setFriends(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      {friends.map((friend) => (
+        <div key={friend.id}>
+          <p>name: {friend.name}</p>
+          <p>age: {friend.age}</p>
+          <p>email: {friend.email}</p>
+        </div>
+      ))}
+      <AddFriend />
+    </div>
+  );
+}
+
+export default FriendsList;
---
+++ friends/src/components/Login.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+function Login(props) {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChanges = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/login", credentials)
+      .then((res) => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/protected");
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div>
+      <form onSubmit={login}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          value={credentials.username}
+          onChange={handleChanges}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={credentials.password}
+          onChange={handleChanges}
+        />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
